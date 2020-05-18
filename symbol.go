dcf4cf88e2d68790df5e4d9525c636e19cb632f1
d-tsuji <dram.dt.shonan@gmail.com>
2020-05-19 08:19:13 +0900

    add: symbol

---
+++ symbol.go
@@ -0,0 +1,34 @@
+package gosdlisp
+
+var symbolTable map[string]*Symbol
+
+func init() {
+	symbolTable = make(map[string]*Symbol)
+
+	// Initialization of Symbol T
+	symbolT := &Symbol{name: "T"}
+	symbolT.value = symbolT
+	symbolTable["T"] = symbolT
+
+	// Initialization of Symbol QUIT
+	symbolQuit := &Symbol{name: "QUIT"}
+	symbolTable["QUIT"] = symbolQuit
+}
+
+type Symbol struct {
+	name     string
+	value    T
+	function T
+}
+
+func NewSymbol(name string) *Symbol {
+	symbol, exists := symbolTable[name]
+	if !exists {
+		symbolTable[name] = &Symbol{name: name}
+	}
+	return symbol
+}
+
+func (s *Symbol) String() string {
+	return s.name
+}
