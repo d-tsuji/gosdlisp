76cf354d5d72e9cc2a33dc0778987fe2bbe76ca2
d-tsuji <dram.dt.shonan@gmail.com>
2020-05-20 08:31:55 +0900

    wip: implement functions

--- function.go
+++ function.go
@@ -1,1 +1,35 @@
 package gosdlisp
+
+func init() {
+	AddSymbolFunc("CAR", &Car{})
+	AddSymbolFunc("CDR", &Cdr{})
+}
+
+type Function interface {
+	Atom
+	funCall(arguments List) T
+}
+
+func regist(name string, fun Function) {
+	AddSymbolFunc(name, fun)
+}
+
+type Car struct{}
+
+func (c Car) String() string {
+	return "#<SYSTEM-FUNCTION Car>"
+}
+
+func (c *Car) funCall(arguments List) T {
+	return nil
+}
+
+type Cdr struct{}
+
+func (c Cdr) String() string {
+	return "#<SYSTEM-FUNCTION Cdr>"
+}
+
+func (c *Cdr) funCall(arguments List) T {
+	return nil
+}
