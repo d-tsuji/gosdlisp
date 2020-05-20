a5c996e28638c33f9ef33143c57636013957e759
d-tsuji <dram.dt.shonan@gmail.com>
2020-05-20 23:17:36 +0900

    add: implement bindEvalBody

--- eval.go
+++ eval.go
@@ -56,6 +56,56 @@ func (e Eval) Evaluate(form T) T {
 	return nil
 }
 
+func (e Eval) bindEvalBody(lambda, body, form *Cons) T {
+	// (1) Argument evaluation in a pre-bound environment
+	// (using a temporary stack to store evaluated values)
+	oldStackP := e.stackP
+	for {
+		ret := e.Evaluate(form.Car)
+		e.stack[e.stackP] = ret
+		e.stackP++
+		if form.Cdr == nil {
+			break
+		}
+		form = form.Cdr.(*Cons)
+	}
+
+	// (2) Binding (move the past value of the symbol to the stack
+	// and put the value evaluated in (1) into the symbol (swap))
+	argList := lambda
+	sp := oldStackP
+	for {
+		sym := (argList.Cdr).(*Symbol)
+		swap := sym.Value
+		sym.Value = e.stack[sp]
+		e.stack[sp] = swap
+		sp++
+		if argList.Cdr == nil {
+			break
+		}
+		argList = (argList.Cdr).(*Cons)
+	}
+
+	// Evaluate body
+	ret := e.evalBody(body)
+
+	// Return the previous value from the stack
+	argList = lambda
+	e.stackP = oldStackP
+	for {
+		sym := (argList.Cdr).(*Symbol)
+		sym.Value = e.stack[oldStackP]
+		oldStackP++
+		if argList.Cdr == nil {
+			break
+		}
+		argList = (argList.Cdr).(*Cons)
+	}
+
+	// return value
+	return ret
+}
+
 func (e Eval) evalBody(body *Cons) T {
 	var ret T
 	for {
