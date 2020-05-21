cb301982e6d47ea45216088ed317f98eabfa3e30
d-tsuji <dram.dt.shonan@gmail.com>
2020-05-21 09:02:53 +0900

    refactoring: file structure

--- base.go
+++ base.go
@@ -1,5 +1,10 @@
 package gosdlisp
 
+import (
+	"log"
+	"strings"
+)
+
 type T interface {
 	String() string
 }
@@ -15,4 +20,42 @@ type List interface {
 type Atom interface {
 	T
 	A()
+}
+
+type Cons struct {
+	Car T
+	Cdr T
+}
+
+func NewCons(car, cdr T) *Cons {
+	return &Cons{car, cdr}
+}
+
+func (c Cons) String() string {
+	var str strings.Builder
+	str.WriteString("(")
+
+	list := &c
+
+	for {
+		str.WriteString(list.Car.String())
+		if list.Cdr == nil {
+			str.WriteString(")")
+			break
+		} else if a, ok := list.Cdr.(Atom); ok {
+			str.WriteString(" . ")
+			str.WriteString(a.String())
+			str.WriteString(")")
+			break
+		} else {
+			str.WriteString(" ")
+			l, ok := list.Cdr.(*Cons)
+			if !ok {
+				log.Fatalf("cannot convert Cons: %v", list.Cdr)
+			}
+			list = l
+		}
+	}
+
+	return str.String()
 }
