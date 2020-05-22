344875c40facc2b7e52f3c1bae2f7f25cd569571
d-tsuji <dram.dt.shonan@gmail.com>
2020-05-22 09:29:20 +0900

    update: add test name in reader test

--- reader_test.go
+++ reader_test.go
@@ -13,24 +13,10 @@ func TestReader_read(t *testing.T) {
 		input string
 		want  T
 	}{
-		{name: "", input: `(+ 1 2)`, want: &Cons{
-			Car: &Symbol{
-				Name:     "+",
-				Value:    nil,
-				Function: &Add{},
-			},
-			Cdr: &Cons{
-				Car: &Integer{
-					Value: 1,
-				},
-				Cdr: &Cons{
-					Car: &Integer{
-						Value: 2,
-					},
-					Cdr: nil,
-				},
-			},
-		}},
+		{name: "read arithmetic operations", input: `(+ 1 2)`, want: &Cons{
+			Car: &Symbol{Name: "+", Value: nil, Function: &Add{}},
+			Cdr: &Cons{Car: &Integer{1}, Cdr: &Cons{Car: &Integer{Value: 2}}}},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
